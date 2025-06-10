9151017bc319b01c7d9ea59789ca22d644a3c4ec
Billboard-SP <A00312828@mycambrian.ca>
2025-06-09 21:27:37 -0400

    started Cupcake consturctor

---
+++ GME1011A3/Cupcake.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GME1011A3
+{
+    internal class Cupcake : Minion
+    {
+        //Constructor
+        public Cupcake(int health, int armor, int frosting) : base(health, armor)
+        {
+
+        }
+    }
+}
